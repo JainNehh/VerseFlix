ed6850fe852f211c92be8a96d1ad8d3a1e7bfea8
JainNehh <jnehal1107@gmail.com>
2025-02-11 12:04:46 +0530

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,19 +4,22 @@ function Home() {
   return (
     <div>
 
+<div className='flex '>
      <div className='h-[250px] w-[500px] bg-white m-5 flex-row items-center justify-center'>
-      <img src="" alt="" />
-
+      <img src="src/assets/Corousel/skyforce.jpg" alt="loading..." />
+     </div>
+ 
+     <div className='h-[250px] w-[500px] bg-white m-5 flex-row items-center justify-center '>
+      <img className='' src="src/assets/Corousel/mr-mrs-mahi.jpg" alt="loading..." />
      </div>
 
+     <div className='h-[310px] w-[500px] bg-white m-5 flex-row items-center justify-center '>
+      <img className='h-[310px]' src="src/assets/Corousel/mam.jpg" alt="loading..." />
+     </div>
 
-
-
-
-
-
+     </div>
       <div className='flex items-center justify-center p-52'>
-        <input type="text"  placeholder='Search Anything :) ...'className='bg-white text-black p-3 w-[75%] rounded-3xl font-serif'></input>
+        <input type="text"  placeholder='Search Anything :) ...'className='bg-white text-black p-3 w-[75%] rounded-3xl font-serif '></input>
         <button className='bg-fuchsia-200 text-black p-3 rounded-3xl w-30 font-bold font-serif '>Search</button>
       </div>
     </div>
