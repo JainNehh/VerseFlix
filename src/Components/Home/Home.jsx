2706d8f2a43b7cb82497b61bc590d17f2c970bbc
JainNehh <jnehal1107@gmail.com>
2025-02-11 14:23:18 +0530

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,10 +18,50 @@ function Home() {
      </div>
 
      </div>
-      <div className='flex items-center justify-center p-52'>
+      <div className='flex p-20 items-center justify-center'>
         <input type="text"  placeholder='Search Anything :) ...'className='bg-white text-black p-3 w-[75%] rounded-3xl font-serif '></input>
         <button className='bg-fuchsia-200 text-black p-3 rounded-3xl w-30 font-bold font-serif '>Search</button>
       </div>
+
+
+   <div>
+    <h1 className=' text-4xl font-serif text-center font-bold'>Most Popular Actor</h1>
+
+   <div className='flex gap-10 items-center justify-center p-[50px]'>
+     
+     <div>
+      <img className='h-[150px] w-[150px] rounded-full aspect-square' src="src/assets/Actors/genelia.jpg" alt="loading.." />
+     </div>
+
+     <div>
+      <img className='h-[150px] w-[150px] rounded-full aspect-square' src="src/assets/Actors/shahruk.jpg" alt="loading.." />
+     </div>
+     <div>
+      <img className='h-[150px] w-[150px] rounded-full aspect-square' src="src/assets/Actors/hritik.jpg" alt="loading.." />
+     </div>
+     <div>
+      <img className='h-[150px] w-[150px] rounded-full aspect-square' src="src/assets/Actors/yash.png" alt="loading.." />
+     </div>
+     <div>
+      <img className='h-[150px] w-[150px] rounded-full aspect-square' src="src/assets/Actors/ranbir.jpg" alt="loading.." />
+     </div>
+     <div>
+      <img className='h-[150px] w-[150px] rounded-full aspect-square' src="src/assets/Actors/rash.jpg" alt="loading.." />
+     </div>
+
+
+
+
+
+   </div>
+
+
+   </div>
+
+
+
+
+
     </div>
   )
 }
